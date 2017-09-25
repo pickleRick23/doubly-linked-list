d66ccfc91d11d352e49b1b31ba787bc3565c86e6
Artem Kononovich <artem.kononovich4@gmail.com>
2017-09-26 00:03:24 +0300

    doubly-linked-list: done with new test

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,99 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+      const head = new Node();
+      const tail = new Node();
+      this._head = head;
+      this._tail = tail;
+      head.next = tail;
+      tail.prev = head;
+      this.length = 0;
+    }
 
-    append(data) {}
+    append(data) {
+      const nd = new Node(data, this._tail.prev, this.tail);
+      this._tail.prev.next = nd;
+      this._tail.prev = nd;
+      this.length += 1;
+      return this;
+    }
 
-    head() {}
+    head() {
+      return this._head.next.data;
+    }
 
-    tail() {}
+    tail() {
+      return this._tail.prev.data;
+    }
 
-    at(index) {}
+    at(index) {
+      let nd = this._head.next;
+      for (let i = 0; i < index; i++) {
+        nd = nd.next;
+      }
+      return nd.data;
+    }
 
-    insertAt(index, data) {}
+    insertAt(index, data) {
+      let nd = this._head.next;
+      for (let i = 0; i < index; i++) {
+        nd = nd.next;
+      }
+      const newnd = new Node(data, nd.prev, nd);
+      nd.prev.next = newnd;
+      nd.prev = newnd;
+      return newnd.data;
+    }
 
-    isEmpty() {}
+    isEmpty() {
+      return !!!this.length;
+    }
 
-    clear() {}
+    clear() {
+      if (!this.isEmpty()) {
+        this._head.next = this._tail;
+        this._tail.prev = this._head;
+        this.length = 0;
+      }
+      return this;
+    }
 
-    deleteAt(index) {}
+    deleteAt(index) {
+      let nd = this._head.next;
+      for (let i = 0; i < index; i++) {
+        nd = nd.next;
+      }
+      nd.prev.next = nd.next;
+      nd.next.prev = nd.prev;
+      this.length -= 1;
+      return this;
+    }
 
-    reverse() {}
+    reverse() {
+      if (this.length > 1) {
+        let hnd = this._head.next, tnd = this._tail.prev, temp;
+        for (let i = 1; i <= this.length / 2; i++) {
+          temp = hnd.data;
+          hnd.data = tnd.data;
+          tnd.data = temp;
+          hnd = hnd.next;
+          tnd = tnd.prev;
+        }
+      }
+      return this;
+    }
 
-    indexOf(data) {}
+    indexOf(data) {
+      let nd = this._head.next;
+      for (let i = 0; i < this.length; i++) {
+        if (nd.data == data) {
+          return i;
+        }
+        nd = nd.next;
+      }
+      return -1;
+    }
 }
 
 module.exports = LinkedList;
